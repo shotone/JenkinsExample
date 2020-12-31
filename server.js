6aa7ba5b5ad0008e8bb993dab623942979f22667
shota.firanashvili.1 <shota.firanashvili.1@btu.edu.ge>
2020-12-31 23:17:50 +0400

    Added Text for rendering

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var http = require('http');
 function onRequest(request, response){
     response.writeHead(200,{'Content-type':'text/plain'});
     response.write('Hello World!!!');
+    response.write("One More text");
     response.end();
 }
 
